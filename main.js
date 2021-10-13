9bae43c9e4de6a0e2f2f691b1aaf60df9f3aef61
dlwlrgkwk82 <dlwlrgkwk82@gmail.com>
2021-10-14 00:27:02 +0900

    add constructor function

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 //Constructor function
+function User(email, birthday){
+    this.email = email;
+    this.birthday = birthday;
+    this.buy = function(item){
+        console.log(`${this.email} bought ${item}`);
+    }
+}
+
+const item = 'plugins';
+const user = new User('heejin@heejin.com', '1900-01-01');
+user.buy(item);
