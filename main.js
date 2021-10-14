dcc6ab4737ae75671c7fef49fbc805da0a710b8c
dlwlrgkwk82 <dlwlrgkwk82@gmail.com>
2021-10-14 23:52:10 +0900

    add class

--- main.js
+++ main.js
@@ -10,3 +10,20 @@ function User(email, birthday){
 const item = 'plugins';
 const user = new User('heejin@heejin.com', '1900-01-01');
 user.buy(item);
+
+//Class
+class People{
+    constructor(lastname, birthdate){
+        this.lastname = lastname;
+        this.birthdate = birthdate;
+    }
+
+    introduce(item){
+        console.log(`${this.lastname} was born in ${this.birthdate} and he/she likes ${item}`);
+    }
+}
+
+const person = new People('heejin', '2000-01-01');
+person.introduce('coding');
+
+//Array.prototype
